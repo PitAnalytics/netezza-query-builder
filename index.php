be695ec946be9bb03d53b532270eaddf5c446efd
BernardoColorado <bernardocoloradodubois.hw@gmail.com>
2020-04-05 15:39:02 -0500

    preparando para subir

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+ini_set('memory_limit', '4000M');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once 'vendor/autoload.php';
+
+use App\Google\BigQuerySingleton as BigQuerySingleton;
+
+$bigQuery = BigQuerySingleton::instanciate(['projectId'=>'estado-de-resultados-266105']);
+
+$bseg = $bigQuery->query(
+"SELECT
+BUKRS,
+KOSTL,
+BLDAT,
+BUDAT,
+SGTXT,
+HKONT,
+BLART,
+DMBTR,
+WRBTR,
+PSWSL,
+PSWBT,
+PRCTR,
+LIFNR,
+KUNNR,
+PROJK,
+DBBLG,
+ZUONR
+FROM
+`estado-de-resultados-266105.bseg_2020.bseg_2020_aio`
+WHERE BUKRS = '5200' 
+ORDER BY CAST(BUDAT AS INT64);");
+
+print_r($bseg);
+
+
