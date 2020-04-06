5ee9118eb319916dc117229e18785f221e265553
BernardoColorado <bernardocoloradodubois.hw@gmail.com>
2020-04-05 19:24:51 -0500

    preparando nuevo esquema de queries

--- index.php
+++ index.php
@@ -9,40 +9,50 @@ require_once 'vendor/autoload.php';
 
 use App\Google\BigQuerySingleton as BigQuerySingleton;
 
-$bigQuery = BigQuerySingleton::instanciate(['projectId'=>'estado-de-resultados-266105']);
+if(isset($_GET['sociedad'])){
 
-$bseg = $bigQuery->query(
-"SELECT
-BUKRS,
-KOSTL,
-BLDAT,
-BUDAT,
-SGTXT,
-HKONT,
-BLART,
-DMBTR,
-WRBTR,
-PSWSL,
-PSWBT,
-PRCTR,
-LIFNR,
-KUNNR,
-PROJK,
-DBBLG,
-ZUONR
-FROM
-`estado-de-resultados-266105.bseg_2020.bseg_2020_aio`
-WHERE BUKRS = '5200' 
-ORDER BY CAST(BUDAT AS INT64);");
-
-foreach($bseg as $line){
-
-  $insertable =  "('".$line["BUKRS"]."','".$line["BLDAT"]."','".$line["BUDAT"]."','".$line["SGTXT"]."','".$line["HKONT"]."','".$line["BLART"]."','".$line["DMBTR"]."','".$line["WRBTR"]."','".$line["PSWSL"]."','".$line["PSWBT"]."','".$line["PROJK"]."','".$line["ZUONR"]."),";
-
-  unset($line);
-
-  echo($insertable);
-  echo('<br/>');
+  die('please set params correctly');
 
 }
+else{
 
+$sociedad = $_GET['sociedad'];
+
+$bigQuery = BigQuerySingleton::instanciate(['projectId'=>'estado-de-resultados-266105']);
+
+  $bseg = $bigQuery->query(
+    "SELECT
+    BUKRS,
+    KOSTL,
+    BLDAT,
+    BUDAT,
+    SGTXT,
+    HKONT,
+    BLART,
+    DMBTR,
+    WRBTR,
+    PSWSL,
+    PSWBT,
+    PRCTR,
+    LIFNR,
+    KUNNR,
+    PROJK,
+    DBBLG,
+    ZUONR
+    FROM
+    `estado-de-resultados-266105.bseg_2020.bseg_2020_aio`
+    WHERE BUKRS = '5200' 
+    ORDER BY CAST(BUDAT AS INT64);");
+
+    echo('INSERT INTO bseg(BUKRS,KOSTL,BLDAT,BUDAT,SGTXT,HKONT,BLART,DMBTR,WRBTR,PSWSL,PSWBT,PROJK,ZUONR)');
+    echo('<br>');
+    foreach($bseg as $line){
+    
+      $insertable =  "('".$line["BUKRS"]."','".$line["KOSTL"]."','".$line["BLDAT"]."','".$line["BUDAT"]."','".$line["SGTXT"]."','".$line["HKONT"]."','".$line["BLART"]."','".$line["DMBTR"]."','".$line["WRBTR"]."','".$line["PSWSL"]."','".$line["PSWBT"]."','".$line["PROJK"]."','".$line["ZUONR"]."'),";
+      unset($line);
+      echo($insertable);
+      echo('<br/>');
+    
+    }
+
+}
