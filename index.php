8db9bdf19841c971e4d83fcaeb96355fa4e4138b
BernardoColorado <bernardocoloradodubois.hw@gmail.com>
2020-04-21 13:31:35 -0500

    quitando esquema viejo de queries

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require_once 'vendor/autoload.php';
 
 use App\Google\BigQuerySingleton as BigQuerySingleton;
 
-if(!isset($_GET['sociedad'])||!isset($_GET['mes'])||!isset($_GET['fraccion'])){
+if(!isset($_GET['sociedad'])||!isset($_GET['mes'])||!isset($_GET['dia'])){
 
   die('por favor agregar todos los campos');
 
@@ -18,63 +18,56 @@ if(!isset($_GET['sociedad'])||!isset($_GET['mes'])||!isset($_GET['fraccion'])){
 else{
 ////
 $sociedad = $_GET['sociedad'];
+$anualidad = $_GET['anualidad'];
 $mes = $_GET['mes'];
-$fraccion =$_GET['fraccion'];
+$dia =$_GET['dia'];
+$seccion = intval($_GET['seccion']);
 ////
 $bigQuery = BigQuerySingleton::instanciate(['projectId'=>'estado-de-resultados-266105']);
 ////
-
-$diaMinimo = "1";
-$diaMaximo = "5";
-
-switch ($fraccion) {
-  case '0':
-    $diaMinimo = "1";
-    $diaMaximo = "5";
-    break;
-  case '1':
-    $diaMinimo = "6";
-    $diaMaximo = "10";
-    break;
-  case '2':
-    $diaMinimo = "11";
-    $diaMaximo = "15";
-    break;
-  case '3':
-    $diaMinimo = "16";
-    $diaMaximo = "20";
-    break;
-  case '4':
-    $diaMinimo = "21";
-    $diaMaximo = "25";
-    break;
-  case '5':
-    $diaMinimo = "26";
-    $diaMaximo = "31";
-    break;
-  default:
-    $diaMinimo = "1";
-    $diaMaximo = "5";
-    break;
-}
-
-$query ="SELECT BUKRS,KOSTL,BLDAT,BUDAT,SGTXT,HKONT,BLART,DMBTR,WRBTR,PSWSL,PSWBT,PRCTR,LIFNR,KUNNR,PROJK,DBBLG,ZUONR,SHKZG,BELNR 
+$query =
+"SELECT 
+  BUKRS, 
+  KOSTL, 
+  BLDAT, 
+  BUDAT, 
+  SGTXT, 
+  HKONT, 
+  BLART, 
+  DMBTR, 
+  WRBTR, 
+  PSWSL, 
+  PSWBT, 
+  PRCTR, 
+  LIFNR, 
+  KUNNR, 
+  PROJK, 
+  DBBLG, 
+  ZUONR, 
+  SHKZG, 
+  BELNR 
 FROM 
 `estado-de-resultados-266105.bseg_2020.bseg_2020_aio` 
 WHERE BUKRS = '".$sociedad."' 
+AND CAST(SUBSTR(BUDAT,1,4) AS INT64) = ".$anualidad." 
 AND CAST(SUBSTR(BUDAT,5,2) AS INT64) = ".$mes." 
-AND CAST(SUBSTR(BUDAT,7,2) AS INT64) BETWEEN  ".$diaMinimo." AND ".$diaMaximo." 
+AND CAST(SUBSTR(BUDAT,7,2) AS INT64) =  ".$dia." 
 ORDER BY CAST(BUDAT AS INT64); ";
 ////
   $bseg = $bigQuery->query($query);
-    $size = count($bseg);
+  $size = count($bseg);
 
-    echo("--"." filas: ".$size."<br/>");
-    echo("--"." mes: ".$mes."<br/>");
-    echo("--"." dias: ".$diaMinimo."-".$diaMaximo."<br/>");
+  echo("--"." sociedad: ".$sociedad."<br/>");
+  echo("--"." filas: ".$size."<br/>");
+  echo("--"." mes: ".$anualidad."<br/>");
+  echo("--"." mes: ".$mes."<br/>");
+  echo("--"." dia: ".$dia."<br/>");
 
     for($i=0; $i<$size; $i++){
 
+      $o=$i+1;
+      $r=$o%$seccion;
+      
       $line = $bseg[$i];
       $curedLine=[];
 
@@ -91,7 +84,14 @@ ORDER BY CAST(BUDAT AS INT64); ";
 
       unset($line);
 
-      $insertable =  "(".
+      if($r===1){
+
+        echo("INSERT INTO crude_bseg(BUKRS,KOSTL,BLDAT,BUDAT,SGTXT,HKONT,BLART,DMBTR,WRBTR,PSWSL,PSWBT,PRCTR,LIFNR,KUNNR,PROJK,DBBLG,ZUONR,SHKZG,BELNR) ");
+        echo('<br/>');
+    
+      }
+
+      $insertable =  "SELECT ".
       $curedLine["BUKRS"].",".  //sociedad
       $curedLine["KOSTL"].",".  //ceco
       $curedLine["BLDAT"].",".  //fecha_documento
@@ -110,15 +110,38 @@ ORDER BY CAST(BUDAT AS INT64); ";
       $curedLine["DBBLG"].",".  //referencia
       $curedLine["ZUONR"].",".  //asignacion
       $curedLine["SHKZG"].",".  //tipo_saldo
-      $curedLine["BELNR"].");"; //numero_documento
+      $curedLine["BELNR"];      //numero_documento
+
+      echo($insertable);
+
+      if($r===0){
+
+        echo(";");
+        echo('<br/>');
+        echo("--------------------------------");
+        echo('<br/>');
+
+      }
+      if($r!==0&&$o===$size){
+
+        echo(";");
+        echo('<br/>');
+        echo("------------------------------------------------------------------------------------------");
+        echo('<br/>');
+        echo("------------------------------------------------------------------------------------------");
+        echo('<br/>');
+
+      }
+      else if($r!==0&&$o!==$size){
+
+        echo(" UNION ALL");
+        echo('<br/>');
+
+      }
 
       unset($line);
       unset($bseg[$i]);
-      echo("INSERT INTO bseg(BUKRS,KOSTL,BLDAT,BUDAT,SGTXT,HKONT,BLART,DMBTR,WRBTR,PSWSL,PSWBT,PRCTR,LIFNR,KUNNR,PROJK,DBBLG,ZUONR,SHKZG,BELNR) ");
-      echo("VALUES ");
-      echo($insertable);
-      echo('<br/>');
-    
+      
     }
-
+    
 }
